83dc291779c8123c117bd0550035f1a2beb12bd1
Ethan Arrowood <ethan.arrowood@gmail.com>
2018-06-16 02:01:20 +1000

    New site (#12)
    * import new site files
    
    * delete old files
    
    * add back index.html
    
    * add node_modules to gitignore
    
    * remove path-prefixing

---
+++ gatsby-config.js
@@ -0,0 +1,42 @@
+module.exports = {
+  siteMetadata: {
+    title: "HackWITus 2018",
+    author: "Ethan Arrowood",
+    description: "HackWITus 2018 official website"
+  },
+  pathPrefix: '/',
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/src/posts`,
+        name: "posts",
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/assets/images`,
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 630,
+            },
+          },
+          "gatsby-remark-copy-linked-files",
+        ],
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sass`
+  ],
+}
