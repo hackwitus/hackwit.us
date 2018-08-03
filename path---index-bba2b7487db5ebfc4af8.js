0c4f3c3020daf4d6a63eb76e20b5913a20be61f4
Ethan Arrowood <ethan.arrowood@gmail.com>
2018-08-02 21:23:58 -0400

    Updates

--- path---index-bba2b7487db5ebfc4af8.js
+++ path---index-bba2b7487db5ebfc4af8.js
@@ -1,2 +1,2 @@
-webpackJsonp([0x81b8806e4260],{312:function(t,a){t.exports={data:{site:{siteMetadata:{title:"HackWITus 2018"}}},pathContext:{}}}});
+webpackJsonp([0x81b8806e4260],{314:function(t,a){t.exports={data:{site:{siteMetadata:{title:"HackWITus 2018"}}},pathContext:{}}}});
 //# sourceMappingURL=path---index-bba2b7487db5ebfc4af8.js.map
