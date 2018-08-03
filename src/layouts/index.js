10bf4a47b21aee9327e6eae4fcc9ac5d624caee9
Ethan Arrowood <ethan.arrowood@gmail.com>
2018-08-02 21:30:37 -0400

    Hot fix broken header due to timeout

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -5,30 +5,11 @@ import Header from "../components/Header";
 import Footer from "../components/Footer";
 
 class Template extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        loading: 'is-loading'
-      }
-    }
-
-    componentDidMount () {
-      this.timeoutId = setTimeout(() => {
-          this.setState({loading: ''});
-      }, 100);
-    }
-
-    componentWillUnmount () {
-      if (this.timeoutId) {
-          clearTimeout(this.timeoutId);
-      }
-    }
-
     render() {
         const { children } = this.props;
 
         return (
-            <div className={`body ${this.state.loading}`}>
+            <div className='body'>
                 <Header location={this.props.location} />
                 {children()}
                 <Footer />
