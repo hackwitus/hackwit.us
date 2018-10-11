34afca3cd121deebcbcf9983b3bb52fc0bd63353
Mike Upton <uptonm@wit.edu>
2018-10-11 18:23:19 -0400

    Added register button to Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -17,6 +17,8 @@ class Header extends React.Component {
             <br /> Ira Allen Building, Parker St.
             <br /> Boston, Massachusetts 02120
             <br /> November 10th-11th
+            <br />
+            <a href="https://goo.gl/forms/yitrnC6ri6ujM8l63" className="button special" style={{marginTop: '15px'}} >Register</a>
           </p>
           {this.props.location.pathname === '/' ? null : <a href="/">Home</a>}
         </div>
