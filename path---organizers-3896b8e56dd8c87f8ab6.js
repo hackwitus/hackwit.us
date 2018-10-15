ecec115e1669dd258ebe159dda2892943649d47e
Mike Upton <uptonm@wit.edu>
2018-10-15 12:10:53 -0400

    Updates

--- path---organizers-3896b8e56dd8c87f8ab6.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([0xd260c19d8525],{312:function(t,e){t.exports={data:{site:{siteMetadata:{title:"HackWITus 2018",author:"Ethan Arrowood"}},markdownRemark:{id:"/Users/ethanarrowood/Documents/HackWITus/hackwit.us/src/posts/organizers/index.md absPath of file >>> MarkdownRemark",html:"<p>HackWITus is organized entirely by students. Meet the incredible team putting this event together!</p>",frontmatter:{title:"Meet the Organizers",date:"June 16, 2018"}}},pathContext:{}}}});
-//# sourceMappingURL=path---organizers-3896b8e56dd8c87f8ab6.js.map
