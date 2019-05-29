a99d218113405bc276e9a136b15faaa78f072487
Shawn Toubeau <shawntoubeau@gmail.com>
2019-05-29 19:47:28 -0400

    Changed title

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,24 +1,24 @@
 module.exports = {
   siteMetadata: {
-    title: "HackWITus 2018",
+    title: "HackWITus 2019",
     author: "Ethan Arrowood",
     description: "HackWITus 2018 official website"
   },
-  pathPrefix: '/',
+  pathPrefix: "/",
   plugins: [
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         path: `${__dirname}/src/posts`,
-        name: "posts",
-      },
+        name: "posts"
+      }
     },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `${__dirname}/src/assets/images`,
-      },
+        path: `${__dirname}/src/assets/images`
+      }
     },
     {
       resolve: `gatsby-transformer-remark`,
@@ -27,12 +27,12 @@ module.exports = {
           {
             resolve: `gatsby-remark-images`,
             options: {
-              maxWidth: 630,
-            },
+              maxWidth: 630
+            }
           },
-          "gatsby-remark-copy-linked-files",
-        ],
-      },
+          "gatsby-remark-copy-linked-files"
+        ]
+      }
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
@@ -56,5 +56,5 @@ module.exports = {
         }
       }
     }
-  ],
-}
+  ]
+};
